babc20fc647125decc6813bcb6db77d3e10eb9b8
xiangruiping <xiangruiping>
2020-06-16 14:55:06 +0800

    css link

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,8 +27,8 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel:'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css'},
-      { rel:'stylesheet', href: './css/mobile.css', media:'only screen and (max-width: 480px)'}, // 路径注意需要使用(开发环境)./ , 打包后的路径 http://127.0.0.1:5500/dist/css/mobile.css
-      { rel:'stylesheet', href: './css/pc.css', media:'only screen and (min-width: 480px)'}
+      { rel:'stylesheet', href: 'http://xiangruiping.cn/dist/css/mobile.css', media:'only screen and (max-width: 480px)'}, // 路径注意需要使用(开发环境)./ , 打包后的路径 http://127.0.0.1:5500/dist/css/mobile.css
+      { rel:'stylesheet', href: 'http://xiangruiping.cn/dist/css/pc.css', media:'only screen and (min-width: 480px)'}
     ],
     script: [{
       src: 'https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.slim.min.js'
@@ -38,10 +38,7 @@ module.exports = {
       src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js'
     }]
   },
-  // server: {
-  //   port: 3000, // 默认3000 也可以改成其它的端口号
-  //   host: '0.0.0.0', // 默认localhost
-  // },
+
   plugins: [
     { src: '@/plugins/element-ui', ssr: true },
     { src: '@/assets/js/iconfont.js', ssr: false } // ssr: Boolean (默认为 true) 如果值为 false，该文件只会在客户端被打包引入
