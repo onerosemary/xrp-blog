21e5ff441e9eac40398882fdec1047c2826f09c7
xiangruiping <xiangruiping>
2020-06-18 14:48:40 +0800

    no

--- config/index.js
+++ config/index.js
@@ -1,4 +1,3 @@
-
 // 客户端, 解决 Server error ，www.xiangruiping.cn 或 xiangruiping.cn 2个域名访问问题
-export const productURI = 'xiangruiping.cn'
+export const productURI = 'https://www.xiangruiping.cn'
 export const devURI = 'http://localhost:4000'
