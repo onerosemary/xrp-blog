d21660f5e41080b54fd249cc7cf3bd2e75d29d23
xiangruiping <xiangruiping>
2020-06-18 14:39:16 +0800

    host1

--- config/index.js
+++ config/index.js
@@ -1,3 +1,10 @@
-// export const productURI = 'https://www.xiangruiping.cn'
-export const productURI = '/'
+
+export const productURI = () => {
+    if(process.server === false){  // 客户端, 解决 Server error ，www.xiangruiping.cn 或 xiangruiping.cn 2个域名访问问题
+        return window.location.host
+    }else {
+        return 'https://www.xiangruiping.cn'
+    }
+    
+}
 export const devURI = 'http://localhost:4000'
